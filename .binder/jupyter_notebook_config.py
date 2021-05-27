c08c77d5d11528f760b8b04bd3a3271beaee7ec5
Fabrice Brito <fabrice.brito@terradue.com>
2021-05-27 17:22:57 +0200

    Delete jupyter_notebook_config.py

--- .binder/jupyter_notebook_config.py
+++
@@ -1,9 +0,0 @@
-c = get_config()
-
-c.FileContentsManager.delete_to_trash=False
-
-c.NotebookApp.ip = '*'
-c.NotebookApp.port = 8888
-c.NotebookApp.open_browser = False
-
-c.NotebookApp.notebook_dir = '/workspace'
